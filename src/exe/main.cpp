299460f987d1c777d30e6b0b4cf0e3bdf8234b47
pablo <tretyak@lsystems.ru>
2021-03-04 11:46:44 +0400

    added cmd args

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -1,12 +1,33 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 #include "../lib/DemodulateLibTypes.h" 
 #include "../lib/DemodulateLibMath.h" 
 
 int main(int argc, char * argv[])
 {
 
+	std::string filePath;
+	char mode = 'd';
+	int freq = 24000;
 
+	if (argc > 2)
+	{
+		for (int i = 1; i != argc - 1; ++i)
+		{
+			//filepath
+			if (argv[i][1] == 'f')
+				filePath = std::string(argv[i + 1]);
+
+			//mode
+			if (argv[i][1] == 'm')
+				mode = argv[i + 1][0];
+
+			//freq (rate)
+			if (argv[i][1] == 'r')
+				freq = std::stoi(std::string(argv[i + 1]));
+		}
+	}
 
 	std::fstream fin("data\\am.flt",std::fstream::binary|std::fstream::in);
 	//std::ofstream fout("E:/stc/1-AM/out.txt");
